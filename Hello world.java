83d0d911264674cf3776adc98d6c649746950aeb
Ndmudn <mdnadeem@evolving.com>
2021-06-12 16:03:45 +0530

    remove Hello world Message

--- Hello world.java
+++ Hello world.java
@@ -1,8 +1,6 @@
 class Demo
 {
     public static void main(String args[]){
-        System.out.println(
-            "Hello World"
-        );
+              System.out.println("Hello Git Demo");
     }
 }
